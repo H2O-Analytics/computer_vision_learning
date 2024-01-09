646bda62e6a0677d77643a0d8df4ae0571220dd4
Taylor Waters <waterstaylor80@hotmail.com>
2024-01-08 22:36:09 -0500

    initial comit

---
+++ Projects/Brain_Tumor_Object_Detection/config.py
@@ -0,0 +1,3 @@
+DATA_PATH = '/Users/tawate/Documents/H2O_Analytics/data/cv_engineer_youtube_data/'
+OUTPUT_PATH = '/Users/tawate/Documents/H2O_Analytics/computer_vision_learning/output'
+PROGRAM_PATH = '/Users/tawate/Documents/H2O_Analytics/computer_vision_learning/CV_Engineer_YouTube_Tutorials/Projects/Brain_Tumor_Object_Detection'
