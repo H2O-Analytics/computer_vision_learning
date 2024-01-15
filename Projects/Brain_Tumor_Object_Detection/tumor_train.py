de2f74aa3d252ed2f68c5766b036f89b6e3e17fc
Taylor Waters <waterstaylor80@hotmail.com>
2024-01-15 15:15:24 -0500

    CVL-17 : Hyper parameter tuning method

--- Projects/Brain_Tumor_Object_Detection/tumor_train.py
+++ Projects/Brain_Tumor_Object_Detection/tumor_train.py
@@ -9,6 +9,15 @@ from ultralytics import YOLO
 save_dir = '/Users/tawate/Documents/H2O_Analytics/computer_vision_learning/Projects/Brain_Tumor_Object_Detection/runs/detect'
 model = YOLO("yolov8n.pt") # build new model
 
+# Perform Hyperparameter Tuning
+model.tune(data = "config.yaml"
+           ,epochs = 20
+           ,iterations=300
+           ,project = save_dir
+           ,plots = False
+           ,save = False
+           ,val = False)
+
 # train the model
 resluts = model.train(data = "config.yaml" 
                       ,epochs = 20
