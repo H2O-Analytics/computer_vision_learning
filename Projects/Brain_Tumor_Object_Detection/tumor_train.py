b2ba2f247e817fdf432f0f9022f94e7461f6be66
Taylor Waters <waterstaylor80@hotmail.com>
2024-01-17 16:55:12 -0500

    CVL-17 : Perform Hyper parameter tunning and find the best model

--- Projects/Brain_Tumor_Object_Detection/tumor_train.py
+++ Projects/Brain_Tumor_Object_Detection/tumor_train.py
@@ -11,8 +11,8 @@ model = YOLO("yolov8n.pt") # build new model
 
 # Perform Hyperparameter Tuning
 model.tune(data = "config.yaml"
-           ,epochs = 20
-           ,iterations=300
+           ,epochs = 10
+           ,iterations=10
            ,project = save_dir
            ,plots = False
            ,save = False
@@ -22,3 +22,30 @@ model.tune(data = "config.yaml"
 resluts = model.train(data = "config.yaml" 
                       ,epochs = 20
                       ,project = save_dir)
+
+# training model from best paramters using the above hyper parameter tuning
+results = model.train(  data = "config.yaml"
+                        ,epochs = 1 #pick whatever you want
+                        ,lr0= 0.00905
+                        ,lrf= 0.00917
+                        ,momentum= 0.839
+                        ,weight_decay= 0.00047
+                        ,warmup_epochs= 3.46935
+                        ,warmup_momentum= 0.95
+                        ,box= 7.29726
+                        ,cls= 0.34167
+                        ,dfl= 1.5
+                        ,hsv_h= 0.015
+                        ,hsv_s= 0.70065
+                        ,hsv_v= 0.32482
+                        ,degrees= 0.0
+                        ,translate= 0.1168
+                        ,scale= 0.59227
+                        ,shear= 0.0
+                        ,perspective= 0.0
+                        ,flipud= 0.0
+                        ,fliplr= 0.5603
+                        ,mosaic= 0.97117
+                        ,mixup= 0.0
+                        ,copy_paste= 0.0
+)
