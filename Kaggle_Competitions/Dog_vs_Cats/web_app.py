f81ada70375dbb07bffa40c70079f4fda2640a0b
Taylor Waters <waterstaylor80@hotmail.com>
2024-02-09 17:24:22 -0500

    CVL-28 : create web app for dog vs cat predicition

--- Kaggle_Competitions/Dog_vs_Cats/web_app.py
+++ Kaggle_Competitions/Dog_vs_Cats/web_app.py
@@ -6,7 +6,7 @@ from ultralytics import YOLO
 
 from web_app_util import classify, set_background
 
-set_background('/Users/tawate/Documents/H2O_Analytics/data/cv_engineer_youtube_data/bg5.png')
+set_background('/Users/tawate/Documents/H2O_Analytics/data/Kaggle/dogs_v_cats/bg5.png')
 
 # set title
 st.title('Dog or Cat Classification')
@@ -21,3 +21,15 @@ file = st.file_uploader('',type=['jpeg','jpg', 'png'])
 model = YOLO('/Users/tawate/Documents/H2O_Analytics/computer_vision_learning/runs/classify/train5/weights/best.pt')
 
 # load 
+if file is not None:
+    image = Image.open(file).convert('RGB')
+    st.image(image, use_column_width = True)
+    # generate classification prediction
+    result = model(image)
+    names_dict = result[0].names
+    probs = result[0].probs.data.tolist()
+    
+    # write classification to gui
+    st.write("I am ", np.max(probs) * 100, "% ", "confident this is a ",names_dict[np.argmax(probs)])
+  
+
